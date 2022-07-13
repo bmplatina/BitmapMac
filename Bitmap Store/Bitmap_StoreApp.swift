675faf72ea7e6510850daa4d77dcd8d1413c040f
Platina <kkgordon.com@gmail.com>
2022-07-13 21:36:02 +0900

    Initial Commit

---
+++ Bitmap Store/Bitmap_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bitmap_StoreApp.swift
+//  Bitmap Store
+//
+//  Created by 재혁 on 2022/07/13.
+//
+
+import SwiftUI
+
+@main
+struct Bitmap_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
