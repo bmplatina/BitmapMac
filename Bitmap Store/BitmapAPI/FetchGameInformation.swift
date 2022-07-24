c1f486ff429f17a18510536ae90bc83983df2273
Platina <kkgordon.com@gmail.com>
2022-07-25 00:02:49 +0900

    Improving Game Distribution View

--- Bitmap Store/BitmapAPI/FetchGameInformation.swift
+++ Bitmap Store/BitmapAPI/FetchGameInformation.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+class exampleGameInfo: Identifiable {
+    var gameIndex: Int = 1
+    var gameTitle: [String] = ["The Humanity", "OX"]
+    var gameDeveloper: [String] = ["입학했더니 무한 팀플과 과제가 쌓여버린 건에 대하여", "Team. Assertive"]
+    var gameImage: [String] = ["TheHumanity", "OX"]
+    var gameDistributor: [String] = ["Bitmap Production", "Bitmap Production"]
+}
--- Bitmap Store/View/ContentView/ESD/GameESD_View.swift
+++ Bitmap Store/View/ContentView/ESD/GameESD_View.swift
@@ -9,77 +9,35 @@ import Foundation
 import SwiftUI
 
 struct GameESD_View: View {
-    var gameTitleForNavigation: String = "Games"
+    let columnLayout = Array(repeating: GridItem(), count: 3)
+    var gameInfo: exampleGameInfo = exampleGameInfo()
     var body: some View {
         ScrollView {
-            VStack(alignment: .leading) {
-                HStack {
-                    Image("bitmapWebLogo")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 100)
-                    VStack(alignment: .leading) {
-                        Text("Download Server: Reachable".localized())
-                            .foregroundColor(.gray)
-                            .bold()
-                            .font(Font.footnote)
-                        Text("Games for You".localized())
-                            .font(Font.largeTitle)
-                            .bold()
+            NavigationView {
+                LazyVGrid(columns: columnLayout) {
+                    ForEach(0..<gameInfo.gameIndex+1, id:\.self) { index in
+                        NavigationLink(destination: GameDetailsView(gameIndex: index)) {
+                            ZStack {
+                                Image(gameInfo.gameImage[index])
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 256)
+                                LinearGradient(gradient: Gradient(colors: [.clear, Color.black.opacity(0.5)]), startPoint: .top, endPoint: .bottom).frame(width:256, height: 362)
+                                VStack(alignment: .leading) {
+                                    // Spacer()
+                                    Text(gameInfo.gameTitle[index])
+                                        .foregroundColor(.white)
+                                        .font(Font.largeTitle)
+                                        .bold()
+                                    // Text("개발: 입학했더니 무한 팀플과 과제가 쌓여버린 건에 대하여\n유통: Bitmap Production")
+                                    Text("개발: \(gameInfo.gameDeveloper[index])\n유통: \(gameInfo.gameDistributor[index])")
+                                        .foregroundColor(.white)
+                                }.padding()
+                            }.cornerRadius(24).padding().shadow(radius: 4)
+                        }
                     }
-                    Spacer()
-                }.padding([.leading, .trailing, .top])
-                GameListView().frame(height: 1000, alignment: .leading)
-            }
-        }
-        .navigationTitle(gameTitleForNavigation.localized())
-    }
-}
-
-struct GameListView: View {
-    var body: some View {
-        NavigationView {
-            List {
-                NavigationLink(destination: GameDetailsView(gameIndex: 0)) {
-                    ZStack {
-                        Image("TheHumanity")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 256)
-                        LinearGradient(gradient: Gradient(colors: [.clear, Color.black.opacity(0.5)]), startPoint: .top, endPoint: .bottom).frame(width:256, height: 362)
-                        VStack(alignment: .leading) {
-                            // Spacer()
-                            Text("The Humanity")
-                                .foregroundColor(.white)
-                                .font(Font.largeTitle)
-                                .bold()
-                            // Text("개발: 입학했더니 무한 팀플과 과제가 쌓여버린 건에 대하여\n유통: Bitmap Production")
-                            Text("개발: Platina\n유통: Bitmap Production")
-                                .foregroundColor(.white)
-                        }.padding()
-                    }.cornerRadius(24).padding().shadow(radius: 4)
-                }
-                // Spacer()
-                NavigationLink(destination: GameDetailsView(gameIndex: 1)) {
-                    ZStack {
-                        Image("OX")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width:256)
-                        LinearGradient(gradient: Gradient(colors: [.clear, Color.black.opacity(0.5)]), startPoint: .top, endPoint: .bottom).frame(width: 256, height: 362)
-                        VStack(alignment: .leading) {
-                            // Spacer()
-                            Text("OX")
-                                .foregroundColor(.white)
-                                .font(Font.largeTitle)
-                                .bold()
-                            Text("개발: Team. Assertive\n유통: Bitmap Production")
-                                .foregroundColor(.white)
-                        }.padding()
-                    }.cornerRadius(24).padding().shadow(radius: 4)
                 }
             }
-            .listStyle(SidebarListStyle())
         }
     }
 }
