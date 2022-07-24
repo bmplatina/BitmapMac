31806bcdeee72745ee41e766bf8524f91b4c8116
Platina <kkgordon.com@gmail.com>
2022-07-25 02:23:17 +0900

    .

--- Bitmap Store/BitmapAPI/FetchGameInformation.swift
+++ Bitmap Store/BitmapAPI/FetchGameInformation.swift
@@ -9,8 +9,28 @@ import Foundation
 
 class exampleGameInfo: Identifiable {
     var gameIndex: Int = 1
-    var gameTitle: [String] = ["The Humanity", "OX"]
-    var gameDeveloper: [String] = ["입학했더니 무한 팀플과 과제가 쌓여버린 건에 대하여", "Team. Assertive"]
-    var gameImage: [String] = ["TheHumanity", "OX"]
-    var gameDistributor: [String] = ["Bitmap Production", "Bitmap Production"]
+    var gameTitle: [String] = [
+        "The Humanity",
+        "OX"
+    ]
+    var gameDeveloper: [String] = [
+        "입학했더니 무한 팀플과 과제가 쌓여버린 건에 대하여",
+        "Team. Assertive"
+    ]
+    var gameImage: [String] = [
+        "TheHumanity",
+        "OX"
+    ]
+    var gameDistributor: [String] = [
+        "Interactive Installation, Bitmap Production",
+        "ENTER, Bitmap Production"
+    ]
+    var gameHeadline: [String] = [
+        "오늘 밤, 나는 떠날 것이다",
+        "몰입형 퍼즐 게임 \'OX\'의 세계로"
+    ]
+    var gameDescription: [String] = [
+        "서울예술대학교 인터랙티브 인스톨레이션 출품작. 김달, 이재혁, 정소연 개발",
+        "서울예술대학교 디지털아트 페스티벌 \"ENTER\" 출품작. 김달, 김선진, 유현승, 이재혁, 정소연 개발"
+    ]
 }
--- Bitmap Store/View/ContentView/ESD/GameESD_View.swift
+++ Bitmap Store/View/ContentView/ESD/GameESD_View.swift
@@ -12,9 +12,9 @@ struct GameESD_View: View {
     let columnLayout = Array(repeating: GridItem(), count: 3)
     var gameInfo: exampleGameInfo = exampleGameInfo()
     var body: some View {
-        ScrollView {
-            NavigationView {
-                LazyVGrid(columns: columnLayout) {
+        NavigationView {
+            ScrollView {
+                VStack /* LazyVGrid(columns: columnLayout, alignment: .center, spacing: 16) */ {
                     ForEach(0..<gameInfo.gameIndex+1, id:\.self) { index in
                         NavigationLink(destination: GameDetailsView(gameIndex: index)) {
                             ZStack {
@@ -29,8 +29,7 @@ struct GameESD_View: View {
                                         .foregroundColor(.white)
                                         .font(Font.largeTitle)
                                         .bold()
-                                    // Text("개발: 입학했더니 무한 팀플과 과제가 쌓여버린 건에 대하여\n유통: Bitmap Production")
-                                    Text("개발: \(gameInfo.gameDeveloper[index])\n유통: \(gameInfo.gameDistributor[index])")
+                                    Text(gameInfo.gameDeveloper[index])
                                         .foregroundColor(.white)
                                 }.padding()
                             }.cornerRadius(24).padding().shadow(radius: 4)
@@ -39,78 +38,55 @@ struct GameESD_View: View {
                 }
             }
         }
+        .listStyle(SidebarListStyle())
+        .navigationTitle("Games".localized())
     }
 }
 
-/* struct GameListView: View {
+struct GameDetailsView: View {
+    let gameInfo: exampleGameInfo = exampleGameInfo()
+    @State private var installAlert = false
+    var gameIndex: Int
     var body: some View {
-        HStack {
-            ZStack {
-                Image("TheHumanity")
+        VStack(alignment: .leading) {
+            HStack {
+                Image(gameInfo.gameImage[gameIndex])
                     .resizable()
                     .scaledToFit()
                     .frame(width: 256)
-                LinearGradient(gradient: Gradient(colors: [.clear, Color.black.opacity(0.5)]), startPoint: .top, endPoint: .bottom).frame(width:256, height: 362)
+                    .padding()
                 VStack(alignment: .leading) {
-                    // Spacer()
-                    Text("The Humanity")
-                        .foregroundColor(.white)
+                    Text(gameInfo.gameTitle[gameIndex])
                         .font(Font.largeTitle)
                         .bold()
-                    // Text("개발: 입학했더니 무한 팀플과 과제가 쌓여버린 건에 대하여\n유통: Bitmap Production")
-                    Text("개발: Platina\n유통: Bitmap Production")
-                        .foregroundColor(.white)
+                    Text("Developer".localized() + ": " + gameInfo.gameDeveloper[gameIndex])
+                    Text("Distributor".localized() + ": " + gameInfo.gameDistributor[gameIndex])
                 }.padding()
-            }.cornerRadius(24).padding().shadow(radius: 4)
-            // Spacer()
-            ZStack {
-                Image("OX")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width:256)
-                LinearGradient(gradient: Gradient(colors: [.clear, Color.black.opacity(0.5)]), startPoint: .top, endPoint: .bottom).frame(width: 256, height: 362)
-                VStack(alignment: .leading) {
-                    // Spacer()
-                    Text("OX")
-                        .foregroundColor(.white)
-                        .font(Font.largeTitle)
-                        .bold()
-                    Text("개발: Team. Assertive\n유통: Bitmap Production")
-                        .foregroundColor(.white)
-                }.padding()
-            }.cornerRadius(24).padding().shadow(radius: 4)
-        }
-    }
-} */
-
-struct GameDetailsView: View {
-    @State private var installAlert = false
-    var gameIndex: Int? = nil
-    var body: some View {
-        if gameIndex == 0 {
-            Image("TheHumanity")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 256)
+                
+                Divider()
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        Text(gameInfo.gameHeadline[gameIndex])
+                            .font(Font.largeTitle)
+                            .bold()
+                        Text(gameInfo.gameDescription[gameIndex])
+                    }.padding()
+                }
+            }
             
+            Spacer()
+            
+            Menu("Install Option".localized()) {
+                Button("Install".localized()) {
+                    // installAlert = true
+                }
+                Button("Update".localized()) {
+                }
+                Button("Uninstall".localized()) {
+                }
+            }.padding()
         }
-        else if gameIndex == 1 {
-            Image("OX")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 256)
-        }
-        Menu("Install Option".localized()) {
-            Button("Install") {
-                // installAlert = true
-            }
-            Button("Update") {
-                print("sex")
-            }
-            Button("Uninstall") {
-                print("sex")
-            }
-        }
+        .navigationTitle(gameInfo.gameTitle[gameIndex])
     }
 }
 #if DEBUG
